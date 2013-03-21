ddb7c3a0fd06a2bd77cd036d5c2e1096dbd3e75b
Elliot the Server Bitch <root@elliot.eve-kill.net>
2013-03-21 19:35:01 +0000

    Some cleanup in Social.php

--- classes/Social.php
+++ classes/Social.php
@@ -6,17 +6,13 @@ class Social
 	public static function findConversations()
 	{
 		$timer = new Timer();
-		$maxTime = 65 * 1000;
-		while ($timer->stop() < $maxTime) {
-			$lastSocialTime = Storage::retrieve("Social:lastSocialTime", (time() - (12 * 3600)));
-			$result = Db::query("select killID, unix_timestamp(insertTime) insertTime from zz_killmails where processed = 1 and insertTime >= from_unixtime(:last) order by killID", array(":last" => $lastSocialTime), 0);
-			foreach ($result as $row) {
-				$lastSocialTime = max($lastSocialTime, $row["insertTime"]);
-				Social::beSocial($row["killID"]);
-			}
-			Storage::store("Social:lastSocialTime", $lastSocialTime);
-			sleep(3);
+		$lastSocialTime = Storage::retrieve("Social:lastSocialTime", (time() - (12 * 3600)));
+		$result = Db::query("select killID, unix_timestamp(insertTime) insertTime from zz_killmails where killID > 0 and processed = 1 and insertTime >= from_unixtime(:last) order by insertTime", array(":last" => $lastSocialTime), 0);
+		foreach ($result as $row) {
+			$lastSocialTime = $row["insertTime"];
+			Social::beSocial($row["killID"]);
 		}
+		Storage::store("Social:lastSocialTime", $lastSocialTime);
 	}
 
 	public static function beSocial($killID)
@@ -27,13 +23,13 @@ class Social
 
 		// This is an array of characters we like to laugh at :)
 		$laugh = array(
-			1633218082, // Squizz Caphinator
-			924610627, // fr0gOfWar (petllama)
-			619471207, // Flyboy
-			268946627, // Karbowiak
-			179004085, // Peter Powers
-			428663616, // HyperBeanie (Beansman)
-		);
+				1633218082, // Squizz Caphinator
+				924610627, // fr0gOfWar (petllama)
+				619471207, // Flyboy
+				268946627, // Karbowiak
+				179004085, // Peter Powers
+				428663616, // HyperBeanie (Beansman)
+				);
 
 		$count = Db::queryField("select count(*) count from zz_social where killID = :killID", "count", array(":killID" => $killID), 0);
 		if ($count != 0) return;
@@ -41,12 +37,13 @@ class Social
 		// Get victim info
 		$victimInfo = Db::queryRow("select * from zz_participants where killID = :killID and isVictim = 1", array(":killID" => $killID));
 		$totalPrice = $victimInfo["total_price"];
-		$unixTime = $victimInfo["unix_timestamp"];
+		$dttm = $victimInfo["dttm"];
+		$time = strtotime($dttm);
 
 		if (!in_array($victimInfo["characterID"], $laugh)) { // If in laugh array, skip the checks
 			// Check the minimums, min. price and happened in last 12 hours
 			if ($totalPrice < $ircMin) return;
-			if ($unixTime < (time() - (12 * 3600))) return;
+			if ($time > (time() - (24*3600))) continue;
 		}
 
 		Info::addInfo($victimInfo);
@@ -72,8 +69,10 @@ class Social
 		if ($totalPrice >= $twitMin) {
 			$message .= " #tweetfleet #eveonline";
 			$twit = Twit::sendMessage($message);
-			$twit = "https://twitter.com/eve_kill/status/$twit";
-			Log::irc("Message was also tweeted: |g|$twit");
+			//print_r($twit);
+			//$twit = "https://twitter.com/eve_kill/status/$twit";
+			//Log::irc("Message was also tweeted: |g|$twit");
+			Log::irc("Message was also tweeted: |g|");
 		}
 	}
 }
