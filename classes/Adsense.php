a296790cbaebc9d6b7da0dc5d616b2b4d827279e
Michael Karbowiak <karbowiak@gmail.com>
2013-03-29 22:14:00 +0100

    dick adsense..

--- classes/Adsense.php
+++ classes/Adsense.php
@@ -9,7 +9,7 @@ class Adsense
 {
 	public static function top()
 	{
-		$html = '<script type="text/javascript"><!--
+		$html = '<script type="text/javascript">
 		google_ad_client = "ca-pub-6702479302366531";
 		/* eve-kill */
 		google_ad_slot = "3192421769";
@@ -22,12 +22,13 @@ class Adsense
 
 	public static function bottom()
 	{
-		$html = '<script type="text/javascript"><!--
+		$html = '<script type="text/javascript">
 		google_ad_client = "ca-pub-6702479302366531";
 		/* eve-kill */
 		google_ad_slot = "3192421769";
 		google_ad_width = 728;
 		google_ad_height = 90;
+		</script>
 		<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>';
 		return $html;
 	}
