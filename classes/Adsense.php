b0696b8e637293eb82ab559707c36c27f9a765c9
Michael Karbowiak <karbowiak@gmail.com>
2013-03-29 22:04:05 +0100

    Fixing some retarded adsense crap..

--- classes/Adsense.php
+++ classes/Adsense.php
@@ -15,11 +15,8 @@ class Adsense
 		google_ad_slot = "3192421769";
 		google_ad_width = 728;
 		google_ad_height = 90;
-		//-->
 		</script>
-		<script type="text/javascript"
-		src="//pagead2.googlesyndication.com/pagead/show_ads.js">
-		</script>';
+		<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>';
 		return $html;
 	}
 
@@ -31,10 +28,7 @@ class Adsense
 		google_ad_slot = "3192421769";
 		google_ad_width = 728;
 		google_ad_height = 90;
-		//-->
-		<script type="text/javascript"
-		src="//pagead2.googlesyndication.com/pagead/show_ads.js">
-		</script>';
+		<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>';
 		return $html;
 	}
 	
@@ -46,10 +40,7 @@ class Adsense
 		google_ad_slot = "3192421769";
 		google_ad_width = 728;
 		google_ad_height = 90;
-		//-->
 		</script>
-		<script type="text/javascript"
-		src="//pagead2.googlesyndication.com/pagead/show_ads.js">
-		</script>';
+		<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>';
 	}
 }
