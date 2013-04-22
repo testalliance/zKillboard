a01774dc7b038449002050aecdd6ff34bd6e4f3d
Michael Karbowiak <karbowiak@gmail.com>
2013-04-22 19:17:36 +0200

    Change the license from the Kingboard license derivative thing, to AGPLv3..

--- public/css/README.md
+++ public/css/README.md
@@ -1,5 +1,2 @@
-## Themes
-EDK is limited to span10 in width
-
 # EDK Theme License.
 EDK is licensed under the GPL license. And thus is the files reused for the EDK theme.
--- view/evekillrelatedintercept.php
+++ view/evekillrelatedintercept.php
@@ -1,4 +1,20 @@
 <?php
+/* zKillboard
+ * Copyright (C) 2012-2013 EVE-KILL Team and EVSCO.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 $ek = Db::queryField("SELECT mKillID FROM zz_manual_mails WHERE eveKillID = :id", "mKillID", array(":id" => $id), 0);
 $time = Db::query("SELECT unix_timestamp, solarSystemID FROM zz_participants WHERE killID = :id", array(":id" => "-".$ek), 0);
