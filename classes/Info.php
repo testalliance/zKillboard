3e0ace791e0d2c065f4d218ce8934885e1972ba7
Dr. Jan Itor <root@elliot.eve-kill.net>
2013-05-06 16:07:24 +0000

    Fixed getCharID to prefer current characters over biomassed characters

--- classes/Info.php
+++ classes/Info.php
@@ -315,7 +315,7 @@
 		public static function getCharId($name, $fetchIfNotFound = false)
 		{
 			if (Bin::get("s:$name", null) != null) return Bin::get("s:$name", null);
-			$id = (int)Db::queryField("select characterID from zz_characters where name = :name", "characterID",
+			$id = (int)Db::queryField("select characterID from zz_characters where name = :name order by corporationID desc", "characterID",
 																array(":name" => $name), 30);
 			if ($id == 0 && $fetchIfNotFound) {
 				try {
