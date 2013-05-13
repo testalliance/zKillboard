5c21f417c457823952bbab6f043cd59ad0cd78db
Michael Karbowiak <karbowiak@gmail.com>
2013-05-13 23:46:06 +0200

    Updated stomp.php (stomp reciever) to use the new stomp library, good bye stomp pecl crap!

--- util/stomp.php
+++ util/stomp.php
@@ -17,60 +17,56 @@
  */
 
 require_once dirname(__FILE__) . "/../init.php";
-$logging = false;
 
-global $stompServer, $stompUser, $stompPassword;
+$logging = true;
 
-$stomp = new Stomp($stompServer, $stompUser, $stompPassword);
+global $stompServer, $stompUser, $stompPassword, $baseAddr;
+use FuseSource\Stomp\Stomp;
+$stomp = new Stomp($stompServer);
+$stomp->connect($stompUser, $stompPassword);
 $destination = "/topic/kills";
 $kills = $stomp->subscribe($destination);
 
 while(true)
 {
-	$frame = $stomp->readFrame();
-	if($frame)
-	{
-		$killdata = json_decode($frame->body, true);
-
-		if(!empty($killdata))
-		{
-			$killID = $killdata["killID"];
-			// Check if the killID is already posted (It's always API, so no need to worry about manual mails sneaking in)
-			$count = Db::queryField("SELECT count(1) count FROM zz_killmails WHERE killID = :killID LIMIT 1", "count", array(":killID" => $killID), 0);
-			if($count == 0)
-			{
-				if($killID > 0)
-				{
-					$hash = Util::getKillHash(null, json_decode($frame->body));
-					Db::execute("INSERT IGNORE INTO zz_killmails (killID, hash, source, kill_json) values (:killID, :hash, :source, :json)",
-						array("killID" => $killID, ":hash" => $hash, ":source" => "stompQueue", ":json" => json_encode($killdata)));
-					if($logging)
-						Log::log($frame->headers["message-id"]."::$killID saved");
-					$stomp->ack($frame);
-					continue;
-				}
-				else
-				{
-					if($logging)
-						Log::log($frame->headers["message-id"].":: killID is negative");
-					$stomp->ack($frame);
-					continue;
-				}
-			}
-			else
-			{
-				if($logging)
-					Log::log($frame->headers["message-id"]."::$killID exists");
-				$stomp->ack($frame);
-				continue;
-			}
-		}
-		else
-		{
-			if($logging)
-				Log::log($frame->headers["message-id"].":: frame empty");
-			$stomp->ack($frame);
-			continue;
-		}
-	}
+        if($stomp->hasFrameToRead())
+        {
+                $frame = $stomp->readFrame();
+                if($frame != NULL)
+                {
+                        $killdata = json_decode($frame->body, true);
+                        if(!empty($killdata))
+                        {
+                                $killID = $killdata["killID"];
+                                $count = Db::queryField("SELECT count(1) AS count FROM zz_killmails WHERE killID = :killID LIMIT 1", "count", array(":killID" => $killID), 0);
+                                if($count == 0)
+                                {
+                                        if($killID > 0)
+                                        {
+                                                $hash = Util::getKillHash(null, json_decode($frame->body));
+                                                Db::execute("INSERT IGNORE INTO zz_killmails (killID, hash, source, kill_json) values (:killID, :hash, :source, :json)",
+                                                        array("killID" => $killID, ":hash" => $hash, ":source" => "stompQueue", ":json" => json_encode($killdata)));
+                                                if($logging)
+                                                        Log::log($frame->headers["message-id"]."::$killID saved");
+                                                $stomp->ack($frame);
+                                                continue;
+                                        }
+                                        else
+                                        {
+                                                if($logging)
+                                                        Log::log($frame->headers["message-id"].":: killID is negative");
+                                                $stomp->ack($frame);
+                                                continue;
+                                        }
+                                }
+                                else
+                                {
+                                        if($logging)
+                                                Log::log($frame->headers["message-id"]."::$killID exists");
+                                        $stomp->ack($frame);
+                                        continue;
+                                }
+                        }
+                }
+        }
 }
