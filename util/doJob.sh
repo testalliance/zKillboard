673bace2b3e0f6bd4d0800508e2eea2d92d9bd34
Dr. Jan Itor <root@elliot.eve-kill.net>
2013-05-19 23:38:44 +0000

    removed the util directory

--- util/doJob.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /var/killboard/zkillboard.com/util/
-
-mkdir /tmp/locks/ 2>/dev/null
-
-lockFile=/tmp/locks/$1.lock
-
-flock -w 63 $lockFile php5 doJob.php $1
--- util/recent_stats.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /var/killboard/zkillboard.com/util/
-
-php5 recents_stats.php ; php5 recents_ranks.php
