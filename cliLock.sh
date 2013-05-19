d6ba6b9dc64686192cc41d412fe6fbf88b12d120
Dr. Jan Itor <root@elliot.eve-kill.net>
2013-05-19 21:56:39 +0000

    Bash script for file locking cli commands

---
+++ cliLock.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ] ; then
+	echo Cannot execute an empty command
+	exit 1
+fi
+
+# Make sure we start off in the proper directory
+base=$(dirname $0)
+cd $base
+
+# Create the locks directory
+locks=$base/cache/locks/
+mkdir -p $locks 2>/dev/null
+
+# Determine the lock file
+lockFile=$locks/$1.lock
+
+# Execute!
+flock -w 63 $lockFile php5 $base/cli.php $*
