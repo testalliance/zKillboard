09fc35304278898495e7be3e47c4a996afc3d8f6
Dr. Jan Itor <root@elliot.eve-kill.net>
2013-06-14 15:58:49 +0000

    added edkStyle to DDT

--- install/sql/zz_feeds.sql
+++ install/sql/zz_feeds.sql
@@ -12,9 +12,13 @@ CREATE TABLE `zz_feeds` (
 
   `lastFetchTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
 
+  `edkStyle` tinyint(1) NOT NULL DEFAULT '0',
+
   PRIMARY KEY (`id`),
 
-  UNIQUE KEY `id` (`id`)
+  UNIQUE KEY `id` (`id`),
+
+  KEY `edkStyle` (`edkStyle`)
 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
