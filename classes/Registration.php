e30e3ca95447ea820668e8f87532e5617b85ebc3
Michael Karbowiak <karbowiak@gmail.com>
2013-06-27 20:38:07 +0200

    Hopefully registration is fixed now, hopefully!

--- classes/Registration.php
+++ classes/Registration.php
@@ -31,7 +31,7 @@ class Registration
 		$check = Db::queryField("SELECT count(*) count FROM zz_users WHERE email = :email OR username = :username", "count", array(":email" => $email, ":username" => $username), 0);
 		if ($check == 0) {
 			$hashedpassword = Password::genPassword($password);
-			Db::query("INSERT INTO zz_users (username, password, email) VALUES (:username, :password, :email)", array(":username" => $username, ":password" => $hashedpassword, ":email" => $email));
+			Db::execute("INSERT INTO zz_users (username, password, email) VALUES (:username, :password, :email)", array(":username" => $username, ":password" => $hashedpassword, ":email" => $email));
 			$subject = "zKillboard Registration";
 			$message = "Thank you, $username, for registering at zKillboard.com";
 			Email::send($email, $subject, $message);
