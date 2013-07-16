1863ca2876159ef7e305e8ad397c6ccf50670088
Tincer <Tincer@fakename.com>
2013-07-16 10:36:49 -0400

    Small update - mostly formating a few feature changes * Should fix issues #87 * added documentation for Admin/Moderator class * Removed reset pasword as per Karbowiaks request * moved change email to be admin only ---- Trincer

--- classes/Admin.php
+++ classes/Admin.php
@@ -1,25 +1,55 @@
 <?php
-/*
- - getuserinfo(id)
-  -array info
- - setEmail(id,email)
-  - success or failure
- - setPassword(id)
-  - there new password
- - setRevoked(id,reason)
-  - success or failure
- - setUnRevoked(id)
-  - success of failure
- - setAdmin(id,bool)
- - setModerator(id,bool)
-*/
+/* zKillboard
+ * Copyright (C) 2012-2013 EVE-KILL Team and EVSCO.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * extends Moderator
+ * Various Admin only functions 
+ */
 class Admin extends Moderator
 {
+  /**
+   * Sets/Removes the user admin rights
+   *
+   * @static
+   * @param $userID the userid to change
+   * @param $bool 1= set admin 0 = remove admin
+   */
   public static function setAdmin($userID,$bool){
     Db::execute("UPDATE zz_users SET admin = :access WHERE id = :id", array(":id" => $userID, ":access" => $bool));
   }
+  /**
+   * Sets/Removes the user moderator rights
+   *
+   * @static
+   * @param $userID the userid to change
+   * @param $bool 1 = set admin 0 = remove admin
+   */
   public static function setMod($userID,$bool){
     Db::execute("UPDATE zz_users SET moderator = :access WHERE id = :id", array(":id" => $userID, ":access" => $bool));
+  }
+ /**
+   * Sets the users Email
+   *
+   * @static
+   * @param $userID the userid of the user 
+   * @param $email, the new email address
+   */
+  public static function setEmail($userID,$email){
+    Db::execute("UPDATE zz_users SET email = :email WHERE id = :id",array(":id" => $userID,":email" => $email));
   }
 
 }
--- classes/Moderator.php
+++ classes/Moderator.php
@@ -1,36 +1,53 @@
 <?php
-/*
- - getuserinfo(id)
-  -array info
- - setEmail(id,email)
-  - success or failure
- - setPassword(id)
-  - there new password
- - setRevoked(id,reason)
-  - success or failure
- - setUnRevoked(id)
-  - success of failure
- - setAdmin(id,bool)
- - setModerator(id,bool)
-*/
+/* zKillboard
+ * Copyright (C) 2012-2013 EVE-KILL Team and EVSCO.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Various Moderator Actions
+ *
+ */
 class Moderator
 {
+  /**
+   * Gets the User info
+   *
+   * @static
+   * @param $userID the userid of the user to query
+   * @return The array with the userinfo in it 
+   */
   public static function getUserInfo($userID){
     $info = Db::query("SELECT * FROM zz_users WHERE id = :id", array(":id" => $userID),0); // should this be star
     return $info;
   }
-  public static function setEmail($userID,$email){
-    Db::execute("UPDATE zz_users SET email = :email WHERE id = :id",array(":id" => $userID,":email" => $email));
-  }
-  public static function setPassword($userID){
-    $plaintext = "test";
-    $hpassword = Password::genPassword($plaintext);
-    Db::execute("UPDATE zz_users set password = :password WHERE id = :id",array(":id" => $userID,":password"=> $hpassword));
-    return $plaintext;
-  }
+  /**
+   * Unrevokes the users access 
+   *
+   * @static
+   * @param $userID the userid to change
+   */
   public static function setUnRevoked($userID){
   Db::execute("UPDATE zz_users SET revoked = :access WHERE id = :id", array(":id" => $userID, ":access" => 0));
   }
+  /**
+   * Revokes the users acces
+   *
+   * @static
+   * @param $userID the userid to change
+   * @param $reason the reason why the access was revoked
+   */
   public static function setRevoked($userID,$reason){
     Db::execute("UPDATE zz_users SET revoked = :access WHERE id = :id", array(":id" => $userID, ":access" => 1));
     Db::execute("UPDATE zz_users SET revoked_reason = :reason WHERE id = :id", array(":id" => $userID, ":reason" => $reason));
