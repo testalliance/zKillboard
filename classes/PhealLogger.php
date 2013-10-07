045212905b7e3ecc5cec9d20414a5e472c57b6f8
Dr. Jan Itor <root@elliot.eve-kill.net>
2013-10-07 15:19:21 +0000

    zz_api_log is now a myisam table (again) and we can now do insert delayed on it (again)

--- classes/PhealLogger.php
+++ classes/PhealLogger.php
@@ -25,12 +25,12 @@ class PhealLogger
     public function stop() {}
 
     public function log($scope,$name,$opts) {
-		Db::execute("insert into zz_api_log (scope, name, options) values (:scope, :name, :options)",
+		Db::execute("insert delayed into zz_api_log (scope, name, options) values (:scope, :name, :options)",
 				array(":scope" => $scope, ":name" => $name, ":options" => json_encode($opts)));
 	}
 
     public function errorLog($scope,$name,$opts,$message) {
-		Db::execute("insert into zz_api_log (scope, name, options, errorCode) values (:scope, :name, :options, :error)",
+		Db::execute("insert delayed into zz_api_log (scope, name, options, errorCode) values (:scope, :name, :options, :error)",
 				array(":scope" => $scope, ":name" => $name, ":options" => json_encode($opts), ":error" => substr($message, 0, 127)));
 	}
 }
