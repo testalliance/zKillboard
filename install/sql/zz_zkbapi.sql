1930801cb720bcb0284fdf1d5795b73e1ca96d0a
Dr. Jan Itor <root@elliot.eve-kill.net>
2013-11-06 19:31:11 +0000

    updating ddl

--- install/sql/zz_zkbapi.sql
+++ install/sql/zz_zkbapi.sql
@@ -1,12 +1,14 @@
 
 DROP TABLE IF EXISTS `zz_zkbapi`;
-CREATE TABLE IF NOT EXISTS `zz_zkbapi` (
+CREATE TABLE `zz_zkbapi` (
   `userID` int(11) NOT NULL,
   `keyCode` varchar(64) NOT NULL,
-  `lastAccess` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastAccess` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `accessCount` int(11) NOT NULL,
   `accessLeft` int(11) NOT NULL DEFAULT '5000',
   `additionalAccess` int(11) NOT NULL,
   PRIMARY KEY (`userID`),
   UNIQUE KEY `keyCode` (`keyCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+
