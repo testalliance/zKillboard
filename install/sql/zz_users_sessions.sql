37873554752c8fb4e27753708406835ec5292c3d
Michael Karbowiak <karbowiak@gmail.com>
2013-11-26 20:29:57 +0100

    Add userAgent and IP to the database table

--- install/sql/zz_users_sessions.sql
+++ install/sql/zz_users_sessions.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS `zz_users_sessions` (
   `sessionHash` varchar(192) NOT NULL,
   `dateCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `validTill` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `userAgent` varchar(256) NOT NULL,
+  `ip` varchar(16) NOT NULL,
   UNIQUE KEY `sessionHash` (`sessionHash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
