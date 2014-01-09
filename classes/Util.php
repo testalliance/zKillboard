d6d86927fd610a5bf5aa2664444db5a512b27b4a
Michael Karbowiak <karbowiak@gmail.com>
2014-01-09 19:27:04 +0100

    Added strposa (string search with needle being an array)

--- classes/Util.php
+++ classes/Util.php
@@ -367,5 +367,15 @@ class Util
 			if($val == "." || $val == "..")
 				unset($avail[$key]);
 		return $avail;
-	}	
+	}
+
+	public static function strposa($haystack, $needles=array(), $offset=0) {
+	        $chr = array();
+	        foreach($needles as $needle) {
+	                $res = strpos($haystack, $needle, $offset);
+	                if ($res !== false) $chr[$needle] = $res;
+	        }
+	        if(empty($chr)) return false;
+	        return min($chr);
+	}
 }
