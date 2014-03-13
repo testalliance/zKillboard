fd4c56729932554098d988654283f079c0dcc2c5
root <root@elliot.eve-kill.net>
2014-03-13 21:55:00 +0000

    Latest DDL - includes changes to zz_item_price_lookup

--- install/sql/zz_item_price_lookup.sql
+++ install/sql/zz_item_price_lookup.sql
@@ -3,7 +3,9 @@ DROP TABLE IF EXISTS `zz_item_price_lookup`;
 CREATE TABLE `zz_item_price_lookup` (
   `typeID` int(11) NOT NULL,
   `priceDate` date NOT NULL DEFAULT '0000-00-00',
-  `price` decimal(16,2) NOT NULL,
+  `avgPrice` decimal(16,2) NOT NULL,
+  `lowPrice` decimal(16,2) NOT NULL,
+  `highPrice` decimal(16,2) NOT NULL,
   PRIMARY KEY (`typeID`,`priceDate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
