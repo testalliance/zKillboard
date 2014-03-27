7f3e2fb392115f870285e04e55d5e05b244f44f1
Clinton V. Weiss <cvweiss@gmail.com>
2014-03-27 19:33:03 +0000

    proper zkb ads

--- classes/zKillboard.php
+++ classes/zKillboard.php
@@ -40,12 +40,15 @@ class zKillboard
 		$html = '
 			<script type="text/javascript"><!--
 			google_ad_client = "ca-pub-7481220870937701";
-			google_ad_slot = "9924725708";
+			/* zKillboard */
+			google_ad_slot = "3186136253";
 			google_ad_width = 728;
 			google_ad_height = 90;
 			//-->
 			</script>
-			<script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+			<script type="text/javascript"
+			src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+			</script>
 		';
                 return $html;
         }
