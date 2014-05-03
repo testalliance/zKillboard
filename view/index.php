d6692f4d66a7102683f37b74bffa8e5276c5bae1
Clinton V. Weiss <cvweiss@gmail.com>
2014-05-03 18:38:45 +0000

    domain base

--- view/index.php
+++ view/index.php
@@ -16,22 +16,51 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-$topPoints = array();
-$topIsk = json_decode(Storage::retrieve("TopIsk"), true);
-$topPods = json_decode(Storage::retrieve("TopPods"), true);
-$topPointList = json_decode(Storage::retrieve("TopPoints"), true);
+$serverName = $_SERVER["SERVER_NAME"];
+if ($serverName != "zkillboard.com") {
+	$split = explode(".", $serverName);
+	$board = $split[0];
+	$numDays = 7;
 
-if(is_array($topPointList)) $topPoints = Kills::getKillsDetails($topPointList);
+	$p = array("factionID" => 500001);
 
-$top = array();
-$top[] = json_decode(Storage::retrieve("TopChars"), true);
-$top[] = json_decode(Storage::retrieve("TopCorps"), true);
-$top[] = json_decode(Storage::retrieve("TopAllis"), true);
-$top[] = json_decode(Storage::retrieve("TopShips"), true);
-$top[] = json_decode(Storage::retrieve("TopSystems"), true);
+	$topPoints = array();
+	$topPods = array();
 
-// get latest kills
-$killsLimit = 50;
-$kills = Kills::getKills(array("limit" => $killsLimit));
+	$top = array();
+	$top[] = Info::doMakeCommon("Top Characters", "characterID", Stats::getTopPilots($p));
+	$top[] = Info::doMakeCommon("Top Corporations", "corporationID", Stats::getTopCorps($p));
+	$top[] = Info::doMakeCommon("Top Alliances", "allianceID", Stats::getTopAllis($p));
+	$top[] = Info::doMakeCommon("Top Ships", "shipTypeID", Stats::getTopShips($p));
+	$top[] = Info::doMakeCommon("Top Systems", "solarSystemID", Stats::getTopSystems($p));
 
+	$p["pastSeconds"] = ($numDays*86400);
+	$p["limit"] = 5;
+	$topIsk = Stats::getTopIsk($p);
+	unset($p["pastSeconds"]);
+
+	// get latest kills
+	$killsLimit = 50;
+	$p["limit"] = $killsLimit;
+	$kills = Kills::getKills($p);
+} else {
+	$topPoints = array();
+	$topIsk = json_decode(Storage::retrieve("TopIsk"), true);
+	$topPods = json_decode(Storage::retrieve("TopPods"), true);
+	$topPointList = json_decode(Storage::retrieve("TopPoints"), true);
+
+	if(is_array($topPointList)) $topPoints = Kills::getKillsDetails($topPointList);
+
+	$top = array();
+	$top[] = json_decode(Storage::retrieve("TopChars"), true);
+	$top[] = json_decode(Storage::retrieve("TopCorps"), true);
+	$top[] = json_decode(Storage::retrieve("TopAllis"), true);
+	$top[] = json_decode(Storage::retrieve("TopShips"), true);
+	$top[] = json_decode(Storage::retrieve("TopSystems"), true);
+
+	// get latest kills
+	$killsLimit = 50;
+	$kills = Kills::getKills(array("limit" => $killsLimit));
+
+}
 $app->render("index.html", array("topPods" => $topPods, "topIsk" => $topIsk, "topPoints" => $topPoints, "topKillers" => $top, "kills" => $kills, "page" => 1, "pageType" => "index", "pager" => true));
