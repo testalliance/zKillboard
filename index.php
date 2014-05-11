3e269c42fb16d96c928ae770dbd589a173404e1a
Michael Karbowiak <karbowiak@gmail.com>
2014-05-11 15:47:29 +0200

    Update the index for the installer

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 // Check for a config file, if there is no config, load the installer
-if(!file_exists("config.php"))
-{
+// Also check if a .installed exists, just so we know it's actually installed, even IF the config goes missing..
+//if(!file_exists("config.php"))
+//{
 	include("installer/index.php");
 	die();
-}
+//}
 
 // Include Init
 require_once( "init.php" );
