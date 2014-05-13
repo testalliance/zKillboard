55e04033e3ea8309cb7321388b4d03fd53ab09af
Clinton V. Weiss <cvweiss@gmail.com>
2014-05-13 16:13:10 +0000

    don't index subdomains

---
+++ public/robots.php
@@ -0,0 +1,10 @@
+<?php
+
+$serverName = $_SERVER["SERVER_NAME"];
+
+// Don't index on subdomains
+$disallow = (($serverName == "zkillboard.com") ? "Disallow: /api/" : "Disallow: *");
+
+header("Content-type: text/plain");
+echo "User-agent: *\n";
+echo "$disallow\n";
