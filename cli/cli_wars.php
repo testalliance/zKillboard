7dbcbb249ee95c7078e087057567f8213d95008d
Clinton V. Weiss <cvweiss@gmail.com>
2014-06-03 21:23:35 +0000

    minor tweaks, fixed Db to

--- cli/cli_wars.php
+++ cli/cli_wars.php
@@ -16,14 +16,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 use Perry\Perry;
-use Perry\Setup;
-use Perry\Cache\File\FilePool;
 
 class cli_wars implements cliCommand
 {
 	public function getDescription()
 	{
-		return "";
+		return "Obtain war information and killmails related to the war.";
 	}
 
 	public function getAvailMethods()
@@ -40,13 +38,12 @@ class cli_wars implements cliCommand
 	{
 		$added = 0;
 		$timer = new Timer();
-		$wars = Db::query("select warID from zz_wars where lastChecked < date_sub(now(), interval 1 hour) and (timeFinished is null or timeFinished > date_sub(now(), interval 36 hour)) order by warID desc limit 1000", array(), 0);
+		$wars = $db->query("select warID from zz_wars where lastChecked < date_sub(now(), interval 1 hour) and (timeFinished is null or timeFinished > date_sub(now(), interval 36 hour)) order by warID desc limit 1000", array(), 0);
 		foreach($wars as $war)
 		{
 			if ($timer->stop() > 65000) break; 
 			$id = $war["warID"];
 			$href = "https://public-crest.eveonline.com/wars/$id/";
-			//$warInfo = json_decode(file_get_contents($href), true);
 			$warInfo = Perry::fromUrl($href);
 
 			// Don't fetch killmail api for wars with no kills.. duh
@@ -62,11 +59,12 @@ class cli_wars implements cliCommand
 					$killID = $exploded[4];
 					$hash = $exploded[5];
 
-					$added += Db::execute("insert ignore into zz_crest_killmail (killID, hash) values (:killID, :hash)", array(":killID" => $killID, ":hash" => $hash));
+					$aff = $db->execute("insert ignore into zz_crest_killmail (killID, hash) values (:killID, :hash)", array(":killID" => $killID, ":hash" => $hash));
+					$added += $aff;
 				}
 			}
 
-			Db::execute("update zz_wars set defender = :defender, aggressor = :aggressor, timeDeclared = :timeDeclared, timeStarted = :timeStarted, timeFinished = :timeFinished, agrShipsKilled = :agrShipsKilled, dfdShipsKilled = :dfdShipsKilled, mutual = :mutual, openForAllies = :openForAllies, lastChecked = now() where warID = :warID", array(
+			$db->execute("update zz_wars set defender = :defender, aggressor = :aggressor, timeDeclared = :timeDeclared, timeStarted = :timeStarted, timeFinished = :timeFinished, agrShipsKilled = :agrShipsKilled, dfdShipsKilled = :dfdShipsKilled, mutual = :mutual, openForAllies = :openForAllies, lastChecked = now() where warID = :warID", array(
 						":aggressor" => $warInfo->aggressor->id,
 						":defender" => $warInfo->defender->id,
 						":timeDeclared" => $warInfo->timeDeclared,
