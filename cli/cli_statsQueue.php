ffb0bb5e800d8bfe667c9aa7b36cbec9bae9e8b8
Clinton V. Weiss <cvweiss@gmail.com>
2014-07-02 19:14:47 +0000

    don't call Stomp send unless Stomp is installed

--- cli/cli_statsQueue.php
+++ cli/cli_statsQueue.php
@@ -62,7 +62,7 @@ class cli_statsQueue implements cliCommand
 				}
 				$db->execute("delete from zz_stats_queue where killID = :killID", array(":killID" => $killID));
 				Social::beSocial($killID);
-				StompUtil::sendKill($killID);
+				if (class_exists("Stomp")) StompUtil::sendKill($killID);
 			}
 		}
 	}
