fcaae27c487cbf940666f63aa77063735708ef68
Clinton V. Weiss <cvweiss@gmail.com>
2014-07-16 04:51:39 +0000

    100/100

--- cli/cli_fightFinder.php
+++ cli/cli_fightFinder.php
@@ -39,8 +39,8 @@ class cli_fightFinder implements cliCommand
 		if (!isset($beSocial) || $beSocial == false) return;
 
 		$db->execute("delete from zz_social where insertTime < date_sub(now(), interval 23 hour)");
-		$minPilots = 200;
-		$minWrecks = 200;
+		$minPilots = 100;
+		$minWrecks = 100;
 		$result = $db->query("select * from (select solarSystemID, count(distinct characterID) count, count(distinct killID) kills from zz_participants where characterID != 0 and killID > 0 and dttm > date_sub(now(), interval 1 hour) group by 1 order by 2 desc) f where count >= $minPilots and kills > $minWrecks");
 		foreach($result as $row)
 		{
