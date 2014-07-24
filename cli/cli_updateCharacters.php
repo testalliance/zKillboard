11472f93f2a8b0918c09639320d786e0871c8f89
Clinton V. Weiss <cvweiss@gmail.com>
2014-07-24 13:21:43 +0000

    remove characters with no kills in the database

--- cli/cli_updateCharacters.php
+++ cli/cli_updateCharacters.php
@@ -30,9 +30,7 @@ class cli_updateCharacters implements cliCommand
 
 	public function getCronInfo()
 	{
-		return array(
-			60 => ""
-		);
+		return array(0 => "");
 	}
 
 	public function execute($parameters, $db)
@@ -47,33 +45,45 @@ class cli_updateCharacters implements cliCommand
 			$db->execute("insert ignore into zz_characters (characterID) select ceoID from zz_corporations");
 			$db->execute("insert ignore into zz_characters (characterID) select characterID from zz_api_characters where characterID != 0");
 		}
-		$result = $db->query("select characterID, name from zz_characters where lastUpdated < date_sub(now(), interval 7 day) order by lastUpdated limit 600", array(), 0);
 		$timer = new Timer();
-		foreach ($result as $row) {
-			if (Util::isMaintenanceMode()) return;
-			if (Util::is904Error()) return;
-			$id = $row["characterID"];
-			$db->execute("update zz_characters set lastUpdated = now() where characterID = :id", array(":id" => $id));
-			if ($id >= 2100000000 && $id < 2199999999) continue; // Dust Characters
-			if ($id >= 30000000 && $id <= 31004590) continue; // NPC's
-			if ($id >= 40000000 && $id <= 41004590) continue; // NPC's
+		while ($timer->stop() < 65000) 
+		{
+			$result = $db->query("select characterID, name from zz_characters where lastUpdated < date_sub(now(), interval 7 day) order by lastUpdated limit 100", array(), 0);
+			foreach ($result as $row)
+			{
+				if (Util::isMaintenanceMode()) return;
+				if (Util::is904Error()) return;
+				$id = $row["characterID"];
 
-			$pheal = Util::getPheal();
-			$pheal->scope = "eve";
-			try
-			{
-				$charInfo = $pheal->CharacterInfo(array("characterid" => $id));
-				$name = $charInfo->characterName;
-				$corpID = $charInfo->corporationID;
-				$alliID = $charInfo->allianceID;
-				if ($name != "") $db->execute("update zz_characters set name = :name, corporationID = :corpID, allianceID = :alliID where characterID = :id", array(":id" => $id, ":name" => $name, ":corpID" => $corpID, ":alliID" => $alliID));
-			}
-			catch (Exception $ex)
-			{
+				// Remove characters with no kills
+				$hasKills = $db->queryField("select killID from zz_participants where characterID = :id limit 1", "killID", array(":id" => $id), 0);
+				if ($hasKills === null)
+				{
+					$db->execute("delete from zz_characters where characterID = :id", array(":id" => $id));
+					continue;
+				}
+
+				$db->execute("update zz_characters set lastUpdated = now() where characterID = :id", array(":id" => $id));
+				if ($id >= 2100000000 && $id < 2199999999) continue; // Dust Characters
+				if ($id >= 30000000 && $id <= 31004590) continue; // NPC's
+				if ($id >= 40000000 && $id <= 41004590) continue; // NPC's
+
+				$pheal = Util::getPheal();
+				$pheal->scope = "eve";
+				try
+				{
+					$charInfo = $pheal->CharacterInfo(array("characterid" => $id));
+					$name = $charInfo->characterName;
+					$corpID = $charInfo->corporationID;
+					$alliID = $charInfo->allianceID;
+					if ($name != "") $db->execute("update zz_characters set name = :name, corporationID = :corpID, allianceID = :alliID where characterID = :id", array(":id" => $id, ":name" => $name, ":corpID" => $corpID, ":alliID" => $alliID));
+				}
+				catch (Exception $ex)
+				{
 					Log::log("ERROR Validating Character $id" . $ex->getMessage());
+				}
+				usleep(100000); // Try not to spam the API servers (pauses 1/10th of a second)
 			}
-			usleep(100000); // Try not to spam the API servers (pauses 1/10th of a second)
-			if ($timer->stop() > 60000) return;
 		}
 	}
 }
